f790a54bec4119644d3edeb7d70f8ee04763827c
Arpine Ktoyan <arpine@benekiva.com>
2021-01-20 18:28:21 +0400

    Improvement for Schema

--- server/api/models/Books.js
+++ server/api/models/Books.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
 
 const bookSchema = mongoose.Schema({
-    title:{
+    title: {
         type: String,
         required: true
     },
@@ -10,6 +9,8 @@ const bookSchema = mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Book', bookSchema, 'Books');
