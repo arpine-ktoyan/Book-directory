21f15feef0c20d684ca1c342cc63a345ec283906
Arpine Ktoyan <arpine@benekiva.com>
2021-01-22 17:08:48 +0400

    Update connecting with mongoDB

--- server/index.js
+++ server/index.js
@@ -7,13 +7,16 @@ const dbURI = '';
 
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Successfully connected to the database");
 }).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
+    console.log('Cannot connect to the database. Exiting now...', err);
 });
+
+const db = mongoose.connection;
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("Connection to db established"));
 
 const app = express();
 app.use(express.json());
@@ -25,7 +28,6 @@ app.use('/', itemsRouter);
 app.use('/', (req, res) => {
     res.send('Works!');
 });
-
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
